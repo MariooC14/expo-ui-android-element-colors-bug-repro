b1f9e412964aef8902d1a85c118f95b338c3ffa9
Mario C <mario.caval@icloud.com>
2026-03-15 19:22:37 +0000

    Add date picker element colors demo

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { Host, DateTimePicker } from "@expo/ui/jetpack-compose";
+import { useState } from "react";
+
+const hexDarkColor = "#1e1b16";
+const rgbLightColor = "rgb(255, 0, 0)";
 
 export default function App() {
+  const [containerColor, setContainerColor] = useState(hexDarkColor);
+
+  const toggleDarkMode = () => {
+    setContainerColor((prevColor) =>
+      prevColor === hexDarkColor ? rgbLightColor : hexDarkColor,
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button onPress={toggleDarkMode} title="Toggle Mode"></Button>
+      <Host matchContents>
+        <DateTimePicker
+          displayedComponents="date"
+          variant="picker"
+          elementColors={{ containerColor }}
+        />
+      </Host>
     </View>
   );
 }
@@ -13,8 +33,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
